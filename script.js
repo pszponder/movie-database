25c030905a413e920667dd4b29ad8a437f361bfa
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-05-22 18:48:48 -0600

    update sort function to sort descending by rank

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function createTable(objectList) {
     for (let i = 0; i < objectList.length; i++) {
         table += `
             <tr>
-                <td>${objectList[i].title}</td>
+                <td>${objectList[i].title}</td> 
                 <td>${objectList[i].rank}</td>
                 <td>${objectList[i].id}</td>
             </tr>`;
@@ -87,41 +87,109 @@ function createTable(objectList) {
     document.getElementById("movies-list").innerHTML = table;
 }
 
+// Sort function
+function sort(arr){
+    for (let i = 0; i < arr.length - 1; i++) {
+        // Initialize variables
+        let obj = arr[i];
+        let obj_index = i;
 
-// Create a function to return a sorted array of objects
-// arr is the array that will be sorted
-// property is the property in the array to sort by (title, rank or id)
-// sortDirection determines if you want to sort acending or descending
-function sort(arr, property, sortDirection) {
-    // Iterate over the list n-1 times
-    // For each iteration, establish a 
-}
-
-
-// Create a findMax function which returns the maximum value of an array
-function findMax(arr) {
-    let max = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+        // Iterate across a sub-array and find the max rank
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j].rank > obj.rank) {
+                obj = arr[j];
+                obj_index = j;
+            }
         }
+        // Swap the position of the object with the max value with the object at the current index
+        arr[obj_index] = arr[i];
+        arr[i] = obj;
     }
-    return max;
-}
+    return arr;
+  }
 
-// Create a findMin function which returns the minimum value of an array
-function findMin(arr) {
-    let min = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-    }
-    return min;
-}
+createTable(sort(moviesUnsorted));
+
+// // Create a function to return a sorted array of objects
+// // arr is the array that will be sorted
+// // property is the property in the array to sort by (title, rank or id)
+// // sortDirection determines if you want to sort acending or descending
+// function sort(arr, property, sortDirection) {
+//     // Initialize Variables
+//     let subArray = [];
+//     let value = arr[0].property;
+//     // let propIndex = 0;
+
+//     // // Determine correct index for propIndex (default propIndex is setup for title)
+//     // switch(property) {
+//     //     case 'rank': 
+//     //         propIndex = 1;  // rank
+//     //         break;
+//     //     case 'id':
+//     //         propIndex = 2;  // id
+//     //         break;
+//     //     default: 
+//     //         alert('Please enter a property of either "title", "rank" or "id"');
+//     // }
+
+//     // Iterate over the list n-1 times
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         // For each iteration, establish a sub-array (this will be a subarray of objects)
+//         subArray = arr.slice(i, arr.length);
+
+//         // Compute the max value of the sub-array object and identify the index
+//         // Iterate through each object in the sub-array
+
+//         // Iterate over the objects in subArray
+//         for (let j = 0; j < subArray.length; j++) {
+//             value[0] = findMax(subArray[j], property);
+//             valueIndex[1] = j;
+//         }
+
+//         // value = findMax(subArray[j].property);
+//         // valueIndex = j;
+
+//         // value = findMax(subArray.property);
+//         // valueIndex = findIndex(value);
 
 
 
+//         // Swap the max value with the position of the 1st element in the sub-array
+//         arr[valueIndex] = arr[i];
+//         arr[i] = value;
+//     }
 
-// TODO: Make sure to input the sorted movies list
-createTable(moviesUnsorted);
+// }
+
+
+// // Create a findMax function which returns the maximum value and index of an array of objects
+// function findMax(arr, property) {
+//     let max = arr[0].property;
+//     console.log(max);
+//     let maxIndex = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i].property > max) {
+//             max = arr[i].property;
+//             maxIndex = i;
+//         }
+//     }
+//     return [max, maxIndex];
+// }
+
+
+// // Create a findMin function which returns the minimum value of an array
+// function findMin(arr) {
+//     let min = arr[0];
+//     let minIndex = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] < min) {
+//             min = arr[i];
+//             minIndex = i;
+//         }
+//     }
+//     return [min, minIndex];
+// }
+
+
+// // TODO: Make sure to input the sorted movies list
+// createTable(moviesUnsorted);
